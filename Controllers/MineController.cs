9ec7e5cff724328e94e24af8fe8ad5662a751765
Dylan Chen <hchennyc@outlook.com>
2017-11-23 20:18:51 -0500

    first commit

---
+++ Controllers/MineController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace crypto_converter.Controllers{
+    
+    public class MineController:Controller{
+        private readonly string[] CURRENCIES =  new string[]{
+             "BTC","USD","EUR","CNY","HKD","JPY","AUD","CND","NZD","SGD",
+             "CAD","CLP","GBP","DKK","SEK","ISK","CHF","BRL","RUB"};
+
+        [HttpGet]
+         [Route("/Mine/Get")]
+         public async Task<JsonResult> Get(){
+            var cryptoCurrency = Request.Query["crypto"].ToString().ToUpper();
+            var toCurrency=Request.Query["currencies"].ToString().ToUpper();
+            if (string.IsNullOrEmpty( toCurrency)){
+                toCurrency = string.Join(',',CURRENCIES);
+            }
+            if (string.IsNullOrEmpty(cryptoCurrency)){
+                 throw new Exception ("Missing crypto currency type");
+             }
+             var url = $"https://min-api.cryptocompare.com/data/price?fsym={cryptoCurrency}&tsyms="
+                + toCurrency;
+            var client = new RestSharp.RestClient(url);
+            var req = new RestRequest(Method.GET);
+            req.RequestFormat = DataFormat.Json;
+            req.AddHeader("Accept","application/json");
+            var tcs=new TaskCompletionSource<JsonResult>();
+            var result = new Models.Currencies();
+            client.ExecuteAsync(req, response =>{
+                result = JsonConvert.DeserializeObject<Models.Currencies>(response.Content);
+                var returnObj = new ExpandoObject() as IDictionary<string, Object>;
+                foreach(var prop in result.GetType().GetProperties()){
+                    var val = prop.GetValue(result,null);
+                    if(val is Double && (double)val !=0.0){
+                        returnObj.Add(prop.Name, val);
+                    }
+                }
+                tcs.SetResult(Json(returnObj));
+            });
+            
+            return await tcs.Task;
+         }
+    }
+}
---
+++ Models/Currencies.cs
@@ -0,0 +1,27 @@
+namespace crypto_converter.Models{
+    public class Currencies{
+        public double TWD { get; set; }
+        public double KRW { get; set; }
+        public double THB { get; set; }
+        public double PLN { get; set; }
+        public double BTC { get; set; }
+        public double USD { get; set; }
+        public double EUR { get; set; }
+        public double CNY { get; set; }
+        public double HKD { get; set; }
+        public double JPY { get; set; }
+        public double AUD { get; set; }
+        public double CND { get; set; }
+        public double NZD { get; set; }
+        public double SGD { get; set; }
+        public double CAD { get; set; }
+        public double CLP { get; set; }
+        public double GBP { get; set; }
+        public double DKK { get; set; }
+        public double SEK { get; set; }
+        public double ISK { get; set; }
+        public double CHF { get; set; }
+        public double BRL { get; set; }
+        public double RUB { get; set; }
+    }
+}
